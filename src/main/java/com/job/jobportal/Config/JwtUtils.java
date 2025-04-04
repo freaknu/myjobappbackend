6c93cbc9dfc4123e6f0e6e9d9c4c3c3b977880af
Prabhat Kumar <pk2239.29.jnv@gmail.com>
2025-04-05 00:58:08 +0530

    Updated project with latest changes including render.yaml changessssssss

--- src/main/java/com/job/jobportal/Config/JwtUtils.java
+++ src/main/java/com/job/jobportal/Config/JwtUtils.java
@@ -4,13 +4,13 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,29 +18,28 @@ import java.util.function.Function;
 
 @Component
 public class JwtUtils {
-    private Key signKey;
-
     @Value("${app.secret-key}")
-    public void setSecretKey(String secretKey) {
-        byte[] keyBytes = Base64.getDecoder().decode(secretKey);
-        this.signKey = Keys.hmacShaKeyFor(keyBytes);
-    }
+    private String SECRET_KEY;
 
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         String role = userDetails.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
                 .findFirst()
-                .orElse("USER");  // Default to "USER" if no role found
+                .map(GrantedAuthority::getAuthority)
+                .orElse(null);
 
         claims.put("role", role);
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(signKey, SignatureAlgorithm.HS256)
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
     }
 
     public String extractUsername(String token) {
@@ -62,7 +61,7 @@ public class JwtUtils {
 
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(signKey)
+                .setSigningKey(getSignKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
@@ -72,8 +71,8 @@ public class JwtUtils {
         final String username = extractUsername(token);
         final String tokenRole = extractRole(token);
         final String userRole = userDetails.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
                 .findFirst()
+                .map(GrantedAuthority::getAuthority)
                 .orElse(null);
 
         return username.equals(userDetails.getUsername()) &&
