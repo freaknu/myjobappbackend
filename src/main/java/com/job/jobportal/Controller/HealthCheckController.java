f8dd9cf07c4a6d27a6db269c86d204d20ecf0cbc
Prabhat Kumar <pk2239.29.jnv@gmail.com>
2025-04-05 00:30:31 +0530

    Updated project with latest changes including render.yaml changessssss

--- src/main/java/com/job/jobportal/Controller/HealthCheckController.java
+++ src/main/java/com/job/jobportal/Controller/HealthCheckController.java
@@ -1,9 +1,11 @@
 package com.job.jobportal.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/actuator")
 public class HealthCheckController {
     @GetMapping("/health")
     public String healthCheck() {
