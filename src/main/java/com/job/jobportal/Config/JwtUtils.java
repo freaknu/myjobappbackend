c34d861df78c3f98eea3b1d40caf76d2e36e74de
Prabhat Kumar <pk2239.29.jnv@gmail.com>
2025-04-05 00:36:10 +0530

    Updated project with latest changes including render.yaml changesssssss

--- src/main/java/com/job/jobportal/Config/JwtUtils.java
+++ src/main/java/com/job/jobportal/Config/JwtUtils.java
@@ -18,7 +18,7 @@ import java.util.function.Function;
 
 @Component
 public class JwtUtils {
-    @Value("${app.SECRET_KEY}")
+    @Value("${app.secret-key}")
     private String SECRET_KEY;
 
     public String generateToken(UserDetails userDetails) {
